0d95cf28f2e824aedb8421c129d805eaaab7c003
Teerth67 <157402584+Teerth67@users.noreply.github.com>
2024-07-05 10:18:31 +0530

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://real-time-chat-app-v9zf.onrender.com", {
+			const socket = io("https://chatapp-cgnx.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
